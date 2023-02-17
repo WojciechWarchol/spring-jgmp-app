731ffd9752e075abc5a2addb35e3ef3f22a01f63
Wojciech Warchol <wojciech_warchol@epam.com>
2023-02-17 10:12:58 +0100

    created model interface implementations

---
+++ src/main/java/com/wojto/model/EventImpl.java
@@ -0,0 +1,49 @@
+package com.wojto.model;
+
+import java.util.Date;
+
+public class EventImpl implements Event{
+
+    private long id;
+    private String title;
+    private Date date;
+
+    public EventImpl() {
+    }
+
+    public EventImpl(long id, String title, Date date) {
+        this.id = id;
+        this.title = title;
+        this.date = date;
+    }
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
